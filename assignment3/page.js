08bbeb122b9fa9115e63d350e6e631894551dd2d
njgingrich <njgingrich@gmail.com>
2015-09-13 19:28:57 +0000

    added more styling (selected + hover)

--- assignment3/page.js
+++ assignment3/page.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
   $('#csci245').css('display', 'block');
   $('li').click(function(){
     var divId = $(this).children().attr('href');
-    $(divId).css('display', 'block')
+    $(divId).css('display', 'block');
     $(divId).siblings().css('display', 'none');
+    $(this).css('background-color', '#0c2340');
+    $(this).siblings().css('background-color', '#e35205');
   });
 });
 
